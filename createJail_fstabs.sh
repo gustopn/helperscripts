4898557c42eb12551311166267143bf0d57dd4c6
Lars Schotte <gustik@gustik.eu>
2019-06-09 22:22:45 +0200

    probably working createJail_fstabs.sh

--- createJail_fstabs.sh
+++ createJail_fstabs.sh
@@ -1,13 +1,31 @@
 #!/bin/sh -x
 
+jailsDirPrefixPath="/jail"
 allJailsList=`grep -e '{$' /etc/jail.conf | grep -ve '^#' | awk '{ print $1 }'`
+
+if [ ! -d "$jailsDirPrefixPath" ]
+then \
+  exit 1
+fi
 
 for jailsListInstance in $allJailsList
 do \
-  jailsInstancePath="/etc/fstab.${jailsListInstance}"
-  if [ ! -e "$jailsInstancePath" ]
+  jailsInstanceFstabPath="/etc/fstab.${jailsListInstance}"
+  jailsInstanceRoot="${jailsDirPrefixPath}/${jailsListInstance}"
+  if [ ! -e "$jailsInstanceFstabPath" ]
   then \
-    echo "$jailsInstancePath"
+    touch "$jailsInstanceFstabPath"
+  fi
+  if [ -d "$jailsInstanceRoot" ]
+  then \
+    if [ ! -e "${jailsInstanceRoot}/usr/ports" ]
+    then \
+      mkdir "${jailsInstanceRoot}/usr/ports"
+    fi
+    if [ -d "${jailsInstanceRoot}/usr/ports" ] && [ -d "/usr/ports" ]
+    then \
+      echo "/usr/ports ${jailsInstanceRoot}/usr/ports nullfs ro 0 0" >> "$jailsInstanceFstabPath"
+    fi
   fi
 done
 
